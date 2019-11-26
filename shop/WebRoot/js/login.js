b797cde920eed3e8b8e457fcec1e24a2eba19129
chenyu473 <282237742@qq.com>
2019-11-26 10:18:05 +0800

    js

--- shop/WebRoot/js/login.js
+++ shop/WebRoot/js/login.js
@@ -7,36 +7,36 @@ function login(){
 	console.log(111);
 }
 //点击登录按钮
-$('.login').click(function(){
-	var usernames = $('.username').val();
-	var passwords = $('.password').val();
-	console.log(usernames);
-	console.log(passwords);
-	if(usernames==''){
-		$('.name-tip').html('请输入账号！').siblings('input').css('border-color','#ff6700');
-	};
-	if(passwords==''){
-		$('.pass-tip').html('请输入密码！').siblings('input').css('border-color','#ff6700');
-	};
-	if(usernames!=''&&passwords!=''){
-		$.post('http://www.wjian.top/shop/api_user.php',{
-			status : 'login',
-			username : usernames,
-			password : passwords,
-		}, function(re){
-			console.log(JSON.parse(re));
-			if(code==0){
-				
-			};
-			if(code==1000||code==2002){
-				$('.name-tip').html(result).siblings('input').css('border-color','#ff6700');
-			};
-			if(code==1001){
-				$('.pass-tip').html(result).siblings('input').css('border-color','#ff6700');
-			};
-		});
-	};
-});
+//$('.login').click(function(){
+//	var usernames = $('.username').val();
+//	var passwords = $('.password').val();
+//	console.log(usernames);
+//	console.log(passwords);
+//	if(usernames==''){
+//		$('.name-tip').html('请输入账号！').siblings('input').css('border-color','#ff6700');
+//	};
+//	if(passwords==''){
+//		$('.pass-tip').html('请输入密码！').siblings('input').css('border-color','#ff6700');
+//	};
+//	if(usernames!=''&&passwords!=''){
+//		$.post('http://www.wjian.top/shop/api_user.php',{
+//			status : 'login',
+//			username : usernames,
+//			password : passwords,
+//		}, function(re){
+//			console.log(JSON.parse(re));
+//			if(code==0){
+//				
+//			};
+//			if(code==1000||code==2002){
+//				$('.name-tip').html(result).siblings('input').css('border-color','#ff6700');
+//			};
+//			if(code==1001){
+//				$('.pass-tip').html(result).siblings('input').css('border-color','#ff6700');
+//			};
+//		});
+//	};
+//});
 
 //输入事件
 $('.content>form>div>input').keydown(function(event){
