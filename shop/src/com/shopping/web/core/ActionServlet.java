d0ca781053442b68ce16ef2afaff0ec03b7e59b0
xy <xy@windows10.microdone.cn>
2019-11-30 12:06:12 +0800

    servletAction修复---验证码输出流--outputStream

--- shop/src/com/shopping/web/core/ActionServlet.java
+++ shop/src/com/shopping/web/core/ActionServlet.java
@@ -3,6 +3,7 @@ package com.shopping.web.core;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -170,6 +171,9 @@ public class ActionServlet extends HttpServlet {
          			ArrayList getResult = (ArrayList)o;
                  	JSONArray json  =  JSONArray.fromObject(getResult);
                  	out.write(json.toString());
+         		}else if(o instanceof OutputStream){//为验证码的输出
+         			out = null;
+//         			System.out.println(1111111);
          		}else{
          			JSONObject json = JSONObject.fromObject(o);
          			out.write(json.toString());
